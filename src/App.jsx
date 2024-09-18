7ab57751350199679a3f8eda13ad0376e2eaf767
Batuhanfy <137509436+Batuhanfy@users.noreply.github.com>
2024-09-19 01:20:01 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,17 @@
-
-
-import Header from './components/Header.jsx'
-import About from './components/about.jsx'
-import Mains from './components/Mains.jsx'
-import { Route, Routes } from 'react-router-dom';
-
+import Header from "./components/Header.jsx";
+import About from "./components/about.jsx";
+import Mains from "./components/Mains.jsx";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-
   return (
     <>
       <div>
-       <Header />
-      <Mains/>
-   
-    </div>
-    
+        <Header />
+        <Mains />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
